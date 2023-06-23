06ebfa6b283c830d6f7e5a683a3e15a69d308851
Yun Liu <yunliu@Yuns-MacBook-Pro.local>
2023-06-23 14:37:38 -0400

    add role to authControl response

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,7 @@ const handleLogin = async (req, res) => {
     // evaluate password
     const match = await bcrypt.compare(pwd, foundUser.password);
     if (match) {
-        const roles = Object.values(foundUser.roles);
+        const roles = Object.values(foundUser.roles).filter(Boolean); // remove Null values from roles
         // create JWTs
         const accessToken = jwt.sign(
             {
@@ -34,7 +34,7 @@ const handleLogin = async (req, res) => {
         foundUser.refreshToken = refreshToken;
         const result = await foundUser.save();
         res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); // secure: true,  (for production)
-        res.json({ accessToken });
+        res.json({ roles, accessToken });
     } else {
         res.sendStatus(401);
     }
