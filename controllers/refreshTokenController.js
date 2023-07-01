c30a084235209972e2e573a5f0216ccba36d4b24
yun <caseyunliu@gmail.com>
2023-07-01 12:09:49 -0400

    add roles to refreshtoken response

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -25,7 +25,7 @@ const handleRefreshToken = async (req, res) => {
                 process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: '30s' }
             );
-            res.json({ accessToken })
+            res.json({ roles, accessToken })
         }
     );
 }
